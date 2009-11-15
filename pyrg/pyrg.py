4f7e098b87c4991286b14400ec273fe4b40ed575
Hideo Hattori <devnull@localhost>
2009-11-15 17:45:05 +0900

    featured: color configration

--- pyrg/pyrg.py
+++ pyrg/pyrg.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python
 """pyrg - colorized Python's UnitTest Result Tool"""
+from ConfigParser import ConfigParser
 from subprocess import Popen, PIPE
 from select import poll, POLLIN
 import sys
 import re
+import os
 
 __version__ = '0.2.1'
 __author__ = 'Hideo Hattroi <hhatto.jp@gmail.com>'
 __license__ = 'NewBSDLicense'
 
-OK_COLOR = "[32m%s[0m"
-FAIL_COLOR = "[31m%s[0m"
-ERROR_COLOR = "[33m%s[0m"
-FUNC_COLOR = "[36m%s[0m"
+PRINT_COLOR_SET = {
+        'ok': '[32m%s[0m',
+        'fail': '[31m%s[0m',
+        'error': '[33m%s[0m',
+        'function': '[36m%s[0m',
+}
+COLOR_MAP = {
+        'black': '[30m%s[0m',
+        'red': '[31m%s[0m',
+        'green': '[32m%s[0m',
+        'yellow': '[33m%s[0m',  # TODO:fail??
+        'blue': '[34m%s[0m',
+        'magenta': '[35m%s[0m',
+        'cyan': '[36m%s[0m',
+        'white': '[37m%s[0m',
+        }
 
 
 def parse_result_line(line):
@@ -26,15 +40,19 @@ def parse_result_line(line):
     if err and fail:
         f = line.split('=')[1].split(',')[0]
         e = line.split('=')[2].split(')')[0]
-        result = "(" + FAIL_COLOR % "failures" + "=" + FAIL_COLOR % f + ", "
-        result += ERROR_COLOR % "errors" + "=" + ERROR_COLOR % e + ")"
+        result = "(%s=%s. " % (PRINT_COLOR_SET['fail'] % "failures",
+                               PRINT_COLOR_SET['fail'] % f)
+        result += "%s=%s)" % (PRINT_COLOR_SET['error'] % "errors",
+                              PRINT_COLOR_SET['error'] % e)
     elif fail and not err:
         l = line.split('=')[1].split(')')[0]
-        result = "(" + FAIL_COLOR % "failures" + "=" + FAIL_COLOR % l + ")"
+        result = "(%s=%s)" % (PRINT_COLOR_SET['fail'] % "failures",
+                              PRINT_COLOR_SET['fail'] % l)
     elif err and not fail:
         l = line.split('=')[1].split(')')[0]
-        result = "(" + ERROR_COLOR % "errors" + "=" + ERROR_COLOR % l + ")"
-    return FAIL_COLOR % "FAILED" + " %s" % result
+        result = "(%s=%s)" % (PRINT_COLOR_SET['error'] % "errors",
+                              PRINT_COLOR_SET['error'] % l)
+    return PRINT_COLOR_SET['fail'] % "FAILED" + " %s" % result
 
 
 def parse_lineone(line):
@@ -43,11 +61,11 @@ def parse_lineone(line):
     line = line.strip()
     for char in line:
         if '.' == char:
-            results.append(OK_COLOR % ".")
+            results.append(PRINT_COLOR_SET['ok'] % ".")
         elif 'E' == char:
-            results.append(ERROR_COLOR % "E")
+            results.append(PRINT_COLOR_SET['error'] % "E")
         elif 'F' == char:
-            results.append(FAIL_COLOR % "F")
+            results.append(PRINT_COLOR_SET['fail'] % "F")
         else:
             results.append(char)
     return "".join(results)
@@ -55,7 +73,7 @@ def parse_lineone(line):
 
 def coloring_method(line):
     """colorized method line"""
-    return FUNC_COLOR % line
+    return PRINT_COLOR_SET['function'] % line
 
 
 def parse_unittest_result(lines):
@@ -65,24 +83,45 @@ def parse_unittest_result(lines):
     fail = re.compile("FAIL:")
     ok = re.compile("OK")
     failed = re.compile("FAILED")
-    results.append(parse_lineone(lines[0])+'\n')
+    results.append(parse_lineone(lines[0]) + '\n')
     for line in lines[1:]:
         if ok.match(line):
-            result = OK_COLOR % "OK"
+            result = PRINT_COLOR_SET['ok'] % "OK"
         elif failed.match(line):
             result = parse_result_line(line)
         elif fail.match(line):
-            result = FAIL_COLOR % "FAIL" + ":" + coloring_method(line[5:])
+            result = "%s:%s" % (PRINT_COLOR_SET['fail'] % "FAIL",
+                                coloring_method(line[5:]))
         elif err.match(line):
-            result = ERROR_COLOR % "ERROR" + ":" + coloring_method(line[6:])
+            result = "%s:%s" % (PRINT_COLOR_SET['error'] % "ERROR",
+                                coloring_method(line[6:]))
         else:
             result = line
         results.append(result)
     return "".join(results)
 
 
+def get_configfile_path():
+    """get $HOME/.pyrgrc path"""
+    return "/home/%s/.pyrgrc" % (os.getlogin())
+
+
+def set_configration():
+    """setting to printing color map"""
+    filename = get_configfile_path()
+    if not os.path.exists(filename):
+        print "default configration"
+        return
+    configure = ConfigParser()
+    configure.read(filename)
+    for item in configure.items('color'):
+        PRINT_COLOR_SET[item[0]] = COLOR_MAP[item[1]]
+    return
+
+
 def main():
     """execute command line tool"""
+    set_configration()
     if sys.argv[1:]:
         p = Popen(['python', sys.argv[1]], stdout=PIPE, stderr=PIPE)
         r = p.communicate()[1]
